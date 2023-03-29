e13eb3a0dceb7b7a3444f30b4fb3de4c5389dc9d
KarolinaKuszpa <117316629+KarolinaKuszpa@users.noreply.github.com>
2023-03-29 21:58:49 +0200

    Homework1,2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const ul = document.querySelector(".gallery");
+
+for (const gallery of galleryItems) {
+  const Elementli = `<div class= "gallery__item" href="${gallery.or}">
+  
+  <img 
+  class="gallery__image" 
+  src="${gallery.preview}" 
+  alt="${gallery.description}" />
+</a>
+    </div>`;
+  const listElement = document.createElement("li");
+  listElement.innerHTML = Elementli;
+  ul.appendChild(listElement);
+}
+console.log(galleryItems);
+
+ul.addEventListener("click", (ev) => {
+  ev.preventDefault();
+
+  const instance = window.basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+});
 
 console.log(galleryItems);
